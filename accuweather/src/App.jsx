8be9affec16b0362c8ad43069edea86f20e2f58d
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-08-20 18:20:59 +0600

    footer section

--- accuweather/src/App.jsx
+++ accuweather/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
+import Navbar2 from "./components/Navbar2/Navbar2";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Navbar2 />
+      <Footer />
     </>
   );
 }
