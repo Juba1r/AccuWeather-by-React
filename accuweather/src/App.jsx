045ab7eec7ea4b4128057601e1e7e1d15f7f3aa9
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-08-25 14:51:09 +0600

    hourly created

--- accuweather/src/App.jsx
+++ accuweather/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./components/Homepage/homepage";
+import HourlyWeather from "./components/Hourly/HourlyWeather";
 
 function App() {
   return (
     <div className="main-container">
       <HomePage />
+      <HourlyWeather />
     </div>
   );
 }
